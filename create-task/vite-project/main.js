86bfe1e4f205f4e10d7f0da6987a5014f8752d71
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2023-02-08 10:17:15 -0500

    f

--- create-task/vite-project/main.js
+++ create-task/vite-project/main.js
@@ -1,1 +1,15 @@
-
+async function bars() {
+  try {
+    const response = await fetch(
+      "https://a3odwonexi.execute-api.us-east-2.amazonaws.com/default/Bars_API/"
+    );
+    const amiibosData = await response.json();
+    if (response.status < 200 || response.status > 299) {
+      console.log("poop");
+    }
+    console.log(amiibosData);
+  } catch (error) {
+    console.log(error);
+    console.log("big poo");
+  }
+}
