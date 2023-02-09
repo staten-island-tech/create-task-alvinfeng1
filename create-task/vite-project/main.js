85df24f7e5eee945509e2da78acc200b3dfea92a
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2023-02-09 10:17:09 -0500

    f

--- create-task/vite-project/main.js
+++ create-task/vite-project/main.js
@@ -1,15 +1,33 @@
-async function bars() {
+async function quotes() {
   try {
-    const response = await fetch(
-      "https://a3odwonexi.execute-api.us-east-2.amazonaws.com/default/Bars_API/"
-    );
-    const amiibosData = await response.json();
+    const response = await fetch("https://api.quotable.io/random");
+    const quotes = await response.json();
     if (response.status < 200 || response.status > 299) {
-      console.log("poop");
+      console.log("good");
     }
-    console.log(amiibosData);
+    console.log(quotes);
+    function displayquotes(quotes) {
+      const container = document.querySelector(".container");
+      container.innerHTML = "";
+      quotes.forEach((quotes) => {
+        let author = quotes.author;
+        let quote = quotes.content;
+        const card = container.insertAdjacentHTML(
+          "beforeend",
+          `<div class="card">
+            <h2>Quote: ${quote}</h2>
+            <p>Author: ${author}</p>
+            <p></p>
+            </div>
+          `
+        );
+      });
+    }
+    displayquotes(quotes);
   } catch (error) {
     console.log(error);
     console.log("big poo");
   }
 }
+
+quotes();
