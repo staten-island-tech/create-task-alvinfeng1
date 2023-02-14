d625e79795f2b8b64a79e9652ab149e088de17c5
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2023-02-14 10:12:48 -0500

    h

--- create-task/vite-project/main.js
+++ create-task/vite-project/main.js
@@ -1,33 +1,71 @@
 async function quote() {
   try {
     const response = await fetch("https://api.quotable.io/random");
-    const quotes = await response.json();
+    const quotesdata = await response.json();
     if (response.status < 200 || response.status > 299) {
       console.log("good");
     }
+    const quotes = quotesdata;
     console.log(quotes);
-    function displayquotes(quotes) {
-      const container = document.querySelector(".container");
-      container.innerHTML = "";
-      quotes.forEach((quotes) => {
-        let author = quotes.author;
-        let quote = quotes.content;
-        const card = container.insertAdjacentHTML(
-          "beforeend",
-          `<div class="card">
-            <h2>Quote: ${quote}</h2>
-            <p>Author: ${author}</p>
-            <p></p>
-            </div>
-          `
-        );
-      });
-    }
     displayquotes(quotes);
   } catch (error) {
     console.log(error);
     console.log("big poo");
   }
+}
+
+function displayquotes(quotes) {
+  const container = document.querySelector(".container");
+  container.innerHTML = "";
+  quotes.forEach((quotes) => {
+    let author = quotes.author;
+    let quote = quotes.content;
+    const card = container.insertAdjacentHTML(
+      "beforeend",
+      `<div class="card">
+        <h2>Quote: ${quote}</h2>
+        <p>Author: ${author}</p>
+        <p></p>
+        </div>
+      `
+    );
+  });
+}
+
+function shortquotes(quotes) {
+  const container = document.querySelector(".container");
+  container.innerHTML = "";
+  quotes.forEach((quotes) => {
+    let author = quotes.author;
+    let quote = quotes.content.length < 60;
+    const card = container.insertAdjacentHTML(
+      "beforeend",
+      `<div class="card">
+        <h2>Quote: ${quote}</h2>
+        <p>Author: ${author}</p>
+        <p></p>
+        </div>
+      `
+    );
+  });
+}
+
+function longquotes(quotes) {
+  const container = document.querySelector(".container");
+  container.innerHTML = "";
+  quotes.forEach((quotes) => {
+    let author = quotes.author;
+    let quote = quotes.content.length > 60;
+    const card = container.insertAdjacentHTML(
+      "beforeend",
+      `<div class="card">
+        <h2>Quote: ${quote}</h2>
+        <p>Author: ${author}</p>
+        <p></p>
+        </div>
+      `
+    );
+  });
 }
 
 quote();
