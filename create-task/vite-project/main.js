7842dee5720e5f4f803c18aa644509424689f393
Alvin Feng <113121131+alvinfeng1@users.noreply.github.com>
2023-02-27 09:58:48 -0500

    f

--- create-task/vite-project/main.js
+++ create-task/vite-project/main.js
@@ -1,4 +1,4 @@
-const quoteApiUrl = 'https://api.quotable.io/random';
+const quoteApiUrl = "https://api.quotable.io/random";
 let previousQuotes = [];
 
 const getQuote = async (maxLength) => {
@@ -7,7 +7,10 @@ const getQuote = async (maxLength) => {
   while (!quote) {
     const response = await fetch(quoteApiUrl);
     const data = await response.json();
-    if (data.content.length <= maxLength && !previousQuotes.includes(data.content)) {
+    if (
+      data.content.length <= maxLength &&
+      !previousQuotes.includes(data.content)
+    ) {
       quote = data.content;
       author = data.author;
     }
@@ -17,34 +20,34 @@ const getQuote = async (maxLength) => {
 };
 
 const displayQuote = (quote, author) => {
-  const quoteElement = document.getElementById('quote');
+  const quoteElement = document.getElementById("quote");
   quoteElement.innerText = quote;
-  const authorElement = document.getElementById('author');
+  const authorElement = document.getElementById("author");
   authorElement.innerText = author;
-  const quoteList = document.getElementById('quote-list');
-  const listItem = document.createElement('li');
+  const quoteList = document.getElementById("quote-list");
+  const listItem = document.createElement("li");
   listItem.innerText = `${quote} - ${author}`;
   quoteList.appendChild(listItem);
 };
 
-document.getElementById('short-button').addEventListener('click', () => {
+document.getElementById("short-button").addEventListener("click", () => {
   getQuote(60);
 });
 
-document.getElementById('long-button').addEventListener('click', () => {
+document.getElementById("long-button").addEventListener("click", () => {
   getQuote(1000);
 });
 
-document.getElementById('search').addEventListener('input', () => {
-  const query = document.getElementById('search').value.toLowerCase();
-  const quoteList = document.getElementById('quote-list');
-  const quotes = quoteList.getElementsByTagName('li');
+document.getElementById("search").addEventListener("input", () => {
+  const query = document.getElementById("search").value.toLowerCase();
+  const quoteList = document.getElementById("quote-list");
+  const quotes = quoteList.getElementsByTagName("li");
   for (let i = 0; i < quotes.length; i++) {
     const quote = quotes[i].innerText.toLowerCase();
     if (quote.includes(query)) {
-      quotes[i].style.display = '';
+      quotes[i].style.display = "";
     } else {
-      quotes[i].style.display = 'none';
+      quotes[i].style.display = "none";
     }
   }
 });
